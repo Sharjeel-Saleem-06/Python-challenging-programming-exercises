9f99847affeb8eab16cd4dd80fbf1552a804affc
SharrY111 <145919334+SharrY111@users.noreply.github.com>
2024-07-12 23:59:43 +0500

    day5

---
+++ Day 5.py
@@ -0,0 +1,24 @@
+# # Write a program to find te oldest age among 3 given ages
+
+# def oldestage():
+#     age1=(input('Enter your age'))
+#     age2=(input('Enter your age'))
+#     age3=(input('Enter your age'))
+#     if age1>age2 and age1>age3:
+#             print("age1 is greater ")
+#     elif age2>age1 and age2>age3:
+#             print("age2 is greater ")
+#     else:
+#         print("age3 is greater ")
+#     return True
+
+# print(oldestage())
+
+# # Write a program to convert celcius to fahrenheit
+# def celcius_to_fahrenheit():
+#     celcius=int(input("Enter temp in celcius"))
+#     fahrenheit=(celcius *9/5)+32
+#     print("temp in fahrenheit is",fahrenheit)
+
+# print(celcius_to_fahrenheit())
+
