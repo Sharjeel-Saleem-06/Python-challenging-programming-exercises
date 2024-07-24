5db663cd3187ce3014fcf614e958d42b8dd086e7
SharrY111 <145919334+Sharjeel-Saleem-06@users.noreply.github.com>
2024-07-24 07:54:52 +0500

    Create Revision Day1.py

---
+++ Revision Day1.py
@@ -0,0 +1,25 @@
+# 1. Write a program to find the oldest age among 3 given ages.
+# 2. Write a program to convert Celsius to Fahrenheit.
+# 3. Write a program to swap 2 numbers provided by the user.
+# 4. Write a program to sum 3 given numbers.
+# 5. Write a program to reverse a 4-digit number and check if the reverse sequence is the same as the original input (palindrome check).
+# 6. Write a program to check if the entered number is even or odd.
+# 7. Write a program to check whether the year is a leap year or not.
+# 8. Write a program to find the Euclidean distance between two points.
+# 9. Write a program to ensure whether three angles can form a triangle.
+# 10. Check whether the cost price is greater than the selling price or not (calculate profit/loss).
+# 11. Write a program to find the simple interest.
+# 12. Write a program to find the volume of a cylinder and calculate the cost when the cost of milk is 1 liter of milk is 40 Rs.
+# 13. Write a program to check if the entered number is divisible by 3 and/or 6.
+# 14. Write a program to find the highest and lowest value in a list.
+# 15. Write a program to check whether a string is a palindrome or not.
+# 16. Write a program that will sum all positive numbers in a list.
+# 17. Create a program that takes a sentence and counts the number of words in it.
+# 18. Implement a program that swaps the values of two variables.
+# 19. Given a list of names, concatenate them as a single string separated by spaces.
+# 20. Write a program to check if a string is a pangram.
+# 21. Write a program to check if a number is prime.
+# 22. Create a function that counts the number of vowels in a string.
+# 23. Create a function that counts unique words in a string.
+# 24. Create a function that counts unique letters in a string.
+# 25. Write a program to check words that start with the letter 'A'.
