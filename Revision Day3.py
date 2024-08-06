db4ecc971b9ed6dff641cccb461b44e029f7037b
Muhammad Sharjeel <145919334+Sharjeel-Saleem-06@users.noreply.github.com>
2024-08-06 10:59:52 -0700

    Create Revision Day3.py

---
+++ Revision Day3.py
@@ -0,0 +1,16 @@
+# Create a function that takes a string of a list and returns the list sorted by occurrence
+# Create a function that takes a string of a list and returns the list sorted by length
+# Implement a function that takes two lists and returns their union (all unique items)
+# Create two dictionaries and merge them into one dictionary
+# Write a program to find the most frequent item in a list
+# Implement a function that removes a key-value pair from a dictionary
+# Create a program to check if two sets have any element in common
+# Given two sets, find the union, intersection, and difference between them
+# Create a function that takes a list of dictionaries and sorts them based on a specified key
+# Write a program that finds the average value of all the elements in a list of dictionaries
+# Write a program that reads a text file and prints its contents
+# Create a new text file and write contents in it
+# Read a CSV file and process its data
+# Write a Python program to copy the contents of one text file to another
+# Given a CSV file with student names and scores, find the student with the highest score
+# Implement a function to count the number of words and lines in a text file
